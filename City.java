9ba07568602d221c7f0fe1f7e4c8f4a7eabb7a2d
Chris Harper <clharper42@students.tntech.edu>
2020-06-03 17:11:46 -0500

    Refactored City

--- City.java
+++ City.java
@@ -8,12 +8,22 @@ public class City
 	private int gene;
 	private String name;
 	private ArrayList<CityDistanceTo> cities;
+	public static int numofcities;
+	public static ArrayList<String> citygenes;
 	
 	public City(String name, int gene)
 	{
 		this.name = name;
 		this.gene = gene; //index in initial array
 		cities = new ArrayList<CityDistanceTo>();
+	}
+
+	public void SetCityDistanceToArrayListSize()
+	{
+		for(int i = 0; i < numofcities; i++)
+		{
+			cities.add(new CityDistanceTo(name, 0, citygenes.indexOf(name)));
+		}
 	}
 	
 	public String getName()
@@ -29,12 +39,13 @@ public class City
 	public void addCityDistanceTo(String city_name, int distance, int gene_to)
 	{
 		CityDistanceTo city = new CityDistanceTo(city_name, distance, gene_to);
-		cities.add(city);
+		//cities.add(city);
+		cities.set(citygenes.indexOf(city_name), city);
 	}
 	
 	public int getDistanceTo(String city_name)
 	{
-		Iterator<CityDistanceTo> city_iterator = cities.iterator();
+		/*Iterator<CityDistanceTo> city_iterator = cities.iterator();
 		while(city_iterator.hasNext())
 		{
 			CityDistanceTo temp_city = city_iterator.next();
@@ -42,13 +53,14 @@ public class City
 			{
 				return temp_city.getDistance();
 			}
-		}
-		return 0;
+		}*/
+		return cities.get(citygenes.indexOf(city_name)).getDistance();
+		//return 0;
 	}
 	
 	public int getDistanceTo(int gene_to)
 	{
-		Iterator<CityDistanceTo> city_iterator = cities.iterator();
+		/*Iterator<CityDistanceTo> city_iterator = cities.iterator();
 		while(city_iterator.hasNext())
 		{
 			CityDistanceTo temp_city = city_iterator.next();
@@ -57,7 +69,8 @@ public class City
 				return temp_city.getDistance();
 			}
 		}
-		return 0;
+		return 0;*/
+		return cities.get(gene_to).getDistance();
 	}
 	
 }
